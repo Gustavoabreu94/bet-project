8bd50b7c9413d604c8eeece23b9f8ab76d7fec76
Gustavo Alves Abreu <gustavoalvesabreu94@gmail.com>
2024-09-02 21:47:48 -0300

    Resolved merge conflicts

--- site-project/src/App.tsx
+++ site-project/src/App.tsx
@@ -16,9 +16,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <p>
-        test commit
-      </p>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
